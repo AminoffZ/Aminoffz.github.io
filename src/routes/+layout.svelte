a90223384d360611f1f89b485760974eb6a145cd
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2023-08-21 15:12:27 +0300

    Basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+	import {
+		Footer,
+		FooterCopyright,
+		FooterLink,
+		FooterLinkGroup,
+		NavBrand,
+		NavHamburger,
+		NavLi,
+		NavUl,
+		Navbar
+	} from 'flowbite-svelte';
+	import '../app.postcss';
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">AminoffZ</span
+		>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/docs">Docs</NavLi>
+	</NavUl>
+</Navbar>
+
+<slot />
+
+<Footer>
+	<FooterCopyright href="/" by="AminoffZ" year={2022} />
+	<FooterLinkGroup
+		ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+	>
+		<FooterLink href="/">Privacy Policy</FooterLink>
+		<FooterLink href="/">Licensing</FooterLink>
+		<FooterLink href="/">Contact</FooterLink>
+	</FooterLinkGroup>
+</Footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Gallery } from 'flowbite-svelte';
+</script>
+
+<!-- <Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-3" /> -->
