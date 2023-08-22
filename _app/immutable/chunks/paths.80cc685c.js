bd4b2af0d7fa7234c62ea5f52cfb25eec51470b5
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2023-08-22 15:23:59 +0300

    Updates

---
+++ _app/immutable/chunks/paths.80cc685c.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_v1fhll)==null?void 0:s.base)??"/Aminoffz.github.io";var l;const a=((l=globalThis.__sveltekit_v1fhll)==null?void 0:l.assets)??t;export{a,t as b};
